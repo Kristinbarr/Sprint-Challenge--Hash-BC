229f8d6c7b919d25b5011587d00c0ed324a24aba
Kristin Barr <kristinbarr@gmail.com>
2020-03-13 13:01:38 -0400

    hash tables ex2 completed

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -1,9 +1,5 @@
 #  Hint:  You may not need all of these.  Remove the unused functions.
-from hashtables import (HashTable,
-                        hash_table_insert,
-                        hash_table_remove,
-                        hash_table_retrieve,
-                        hash_table_resize)
+from hashtables import (HashTable, hash_table_insert, hash_table_retrieve)
 
 
 class Ticket:
@@ -16,8 +12,25 @@ def reconstruct_trip(tickets, length):
     hashtable = HashTable(length)
     route = [None] * length
 
-    """
-    YOUR CODE HERE
-    """
+    # put tickets into hashtable, source is key, dest is value
+    for ticket in tickets:
+        hash_table_insert(hashtable, ticket.source, ticket.destination)
 
-    pass
+    trip_list = []
+
+    # retrieve first ticket that has NONE as the source
+    cur_ticket = hash_table_retrieve(hashtable, "NONE")  # LAX
+
+    # loop while destination is not NONE
+    while cur_ticket != "NONE":
+
+        # put ticket into an array
+        trip_list.append(cur_ticket)
+
+        # retrieve the cur_ticket destination to find the matching key
+        next_ticket = hash_table_retrieve(hashtable, cur_ticket)
+
+        # reassign cur_ticket
+        cur_ticket = next_ticket
+
+    return trip_list
