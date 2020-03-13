c3d391aeefb54864d15600765a049639a5568e45
Kristin Barr <kristinbarr@gmail.com>
2020-03-13 14:03:23 -0400

    hashtables ex1 WIP

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -9,11 +9,26 @@ from hashtables import (HashTable,
 def get_indices_of_item_weights(weights, length, limit):
     ht = HashTable(16)
 
-    for weight in weights:
-        
+    # loop through weight arr and insert into hash table
+    for i in range(length):
+        print('\nWEIGHT:', weights[i])
+        # weight is key and index is value
+        hash_table_insert(ht, weights[i], i)
 
+    weight1 = weights[0]
+    weight2 = weights[0]
 
-    return weights
+    # loop through looking for weights
+    while 
+    
+    # if both indexes are the same, no match was found, return None
+    if weight1.value == weight2.value:
+        return None
+    else:
+        # returns a tuple of 2 weights
+        if weight2 > weight1:
+            weight1, weight2 = weight2, weight1
+        return (weight, weight)
 
 
 def print_answer(answer):
