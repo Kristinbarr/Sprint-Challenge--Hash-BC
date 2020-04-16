dc88cf8730de4967c59464f0641f753594a01021
Kristin Barr <kristinbarr@gmail.com>
2020-04-16 15:47:10 -0400

    cs26 updates

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -15,26 +15,33 @@ def get_indices_of_item_weights(weights, length, limit):
         hash_table_insert(ht, weights[i], i)
 
     # save an array and add to the array when a match is found
-    weight_list = []
+    # weight_list = []
 
     # loop through looking for weights
-    for weight in weights:
+    # for weight in weights:
+    for index, weight in enumerate(weights):
+
         weight2 = limit - weight
         
         index2 = hash_table_retrieve(ht, weight2)
-        #  weight2 index is valid and not inside the weight list
-        if index2 and (index2 not in weight_list):
-            weight_list.append(index2)
-        elif index2 and (index2 in weight_list):
-            for i in range(length):
-                if limit - weight2 == weights[i] and (i not in weight_list):
-                    weight_list.append(i)
 
-    if len(weight_list) == 0:
-        return None
-    else:
-        weight_list.sort(reverse=True)
-        return weight_list
+        if index2:
+            return [index2, index]
+
+        #  weight2 index is valid and not inside the weight list
+        # if index2 and (index2 not in weight_list):
+        #     weight_list.append(index2)
+        # elif index2 and (index2 in weight_list):
+            
+        #     for i in range(length):
+        #         if limit - weight2 == weights[i] and (i not in weight_list):
+        #             weight_list.append(i)
+
+    # if len(weight_list) == 0:
+    #     return None
+    # else:
+    #     weight_list.sort(reverse=True)
+    #     return weight_list
 
 
 def print_answer(answer):
